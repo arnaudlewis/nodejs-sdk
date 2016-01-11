0d54aa06bc380f905f900a1250f21a5e1ee926a6
alw <alw@zengularity.com>
2016-01-11 14:38:42 +0100

    split routes and config

--- app.js
+++ app.js
@@ -2,40 +2,9 @@
 /**
  * Module dependencies.
  */
-var express = require('express'),
-    favicon = require('serve-favicon'),
-    logger = require('morgan'),
-    cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser'),
-    methodOverride = require('method-override'),
-    session = require('express-session'),
-    errorHandler = require('errorhandler'),
-    http = require('http'),
-    path = require('path'),
-    prismic = require('express-prismic').Prismic,
-    configuration = require('./prismic-configuration').Configuration;
-
-
-var app = express();
-
-// Prismic.io configuration
-
-prismic.init(configuration);
-
-
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(favicon("public/images/punch.png"));
-app.use(logger('dev'));
-app.use(bodyParser());
-app.use(methodOverride());
-app.use(cookieParser('1234'));
-app.use(session({secret: '1234', saveUninitialized: true, resave: true}));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(errorHandler());
+var prismic = require('express-prismic').Prismic;
+var app = require('./config');
+var PORT = app.get('port');
 
 function handleError(err, req, res) {
   if (err.status == 404) {
@@ -45,15 +14,26 @@ function handleError(err, req, res) {
   }
 }
 
-// Routes
-app.route('/').get(function(req, res){
-  res.render('index');
-});
-
-app.route('/preview').get(prismic.preview);
-
-var PORT = app.get('port');
-
 app.listen(PORT, function() {
   console.log('Express server listening on port ' + PORT);
 });
+
+app.route('/').get(function(req, res) {
+  var latitude = 48.8768767,
+      longitude = 2.3338802,
+      distance = 10 //km;
+
+  var p = prismic.withContext(req, res);
+  p.query(
+    [prismic.Predicates.at("document.type", "store"),
+    prismic.Predicates.near("my.store.location", latitude, longitude, distance)],
+    { pageSize : 10 },
+    function(err, documents) {
+      res.render('index', {
+        prismicdoc: documents.results[0]
+      })
+    }
+  );
+});
+
+app.route('/preview').get(prismic.preview);
